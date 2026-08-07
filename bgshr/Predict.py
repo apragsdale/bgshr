84abedd057d43eda9ba7c3d99cd6a1bb4da98f87
Nicholas <niwicollier@gmail.com>
2026-08-07 08:47:02 -0500

    reverted change to B-map interpolator

--- bgshr/Predict.py
+++ bgshr/Predict.py
@@ -14,7 +14,7 @@ from . import Util, ClassicBGS, Predict
 
 def get_Bmap(xs, Bs):
     """Get a function that interpolates B-values using cubic splines."""
-    return interpolate.PchipInterpolator(xs, Bs)
+    return interpolate.CubicSpline(xs, Bs, bc_type="natural")
 
 
 # -----------------------------------------------------------------------------
